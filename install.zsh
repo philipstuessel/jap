770b92db3715f3a8d1fd719e6ea0d009e03cf6ec
Philip <philip.stuessel@icloud.com>
2024-04-14 21:45:44 +0200

    fix install.zsh

--- install.zsh
+++ install.zsh
@@ -2,10 +2,10 @@
 source ~/.zshrc
 echo "install JAP 🍜"
 echo ${BLUE}">>>>>>>"${NC}
-echo $(mkdir -p ~/jap && curl -o ~/jap/jap.sh https://raw.githubusercontent.com/philipstuessel/jap/main/jap.zsh)
+echo $(mkdir -p ~/jap && curl -o ~/jap/jap.zsh https://raw.githubusercontent.com/philipstuessel/jap/main/jap.zsh)
 echo ${MAGENTA}"install: jap.sh"${NC}
 echo "was installed in: \n"${MAGENTA}$(pwd)${NC}
-echo "\nsource /Users/$(users)/jap/jap.sh" >> ~/.zshrc ""
+echo "\nsource /Users/$(users)/jap/jap.zsh" >> ~/.zshrc ""
 echo ${GREEN}"add in ~/.zshrc"${NC}
 echo $(mkdir -p ~/jap/tpl/) ${MAGENTA}"create: tpl folder"${NC}
 echo $(mkdir -p ~/jap/plugins/) ${MAGENTA}"create: plugins folder"${NC}
