861447acc5fd68d5b5e2fe7bbc253bb89bbc4c74
Philip <philip.stuessel@icloud.com>
2024-04-14 21:47:29 +0200

    fix install.zsh

--- install.zsh
+++ install.zsh
@@ -1,9 +1,21 @@
 #!/bin/zsh
+#color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+NC='\033[0m' # No Color
+
 source ~/.zshrc
 echo "install JAP 🍜"
 echo ${BLUE}">>>>>>>"${NC}
 echo $(mkdir -p ~/jap && curl -o ~/jap/jap.zsh https://raw.githubusercontent.com/philipstuessel/jap/main/jap.zsh)
-echo ${MAGENTA}"install: jap.sh"${NC}
+echo ${MAGENTA}"install: jap.zsh"${NC}
 echo "was installed in: \n"${MAGENTA}$(pwd)${NC}
 echo "\nsource /Users/$(users)/jap/jap.zsh" >> ~/.zshrc ""
 echo ${GREEN}"add in ~/.zshrc"${NC}
